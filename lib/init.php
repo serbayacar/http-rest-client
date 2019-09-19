728d4b6b9702815eb4b7f39028b2b7596af83379
Serbay ACAR <serbay@jotform.com>
2019-09-19 15:12:22 +0300

    PSR-4 :: removed

--- lib/init.php
+++ lib/init.php
@@ -1,30 +1,7 @@
 <?php
 
-spl_autoload_register(function ($class) {
+include_once 'classes/HTTPRestClient.php';
 
-    // project-specific namespace prefix
-    $prefix = '';
-
-    // base directory for the namespace prefix
-    $base_dir = __DIR__ . '/classes/';
-
-    // does the class use the namespace prefix?
-    $len = strlen($prefix);
-    if (strncmp($prefix, $class, $len) !== 0) {
-        // no, move to the next registered autoloader
-        return;
-    }
-
-    // get the relative class name
-    $relative_class = substr($class, $len);
-
-    // replace the namespace prefix with the base directory, replace namespace
-    // separators with directory separators in the relative class name, append
-    // with .php
-    $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
-
-    // if the file exists, require it
-    if (file_exists($file)) {
-        require $file;
-    }
-});
+include_once 'classes/Parser/AbstractParser.php';
+include_once 'classes/Parser/JSON.php';
+include_once 'classes/Parser/PHPArray.php';
